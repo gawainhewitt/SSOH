14f3a6667d1a21c665ab27c5802838d1201b9b7b
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-04-25 11:06:14 +0100

    starttime, endtime, trackgap and number of tracks added as constants

--- constants.h
+++ constants.h
@@ -1,4 +1,9 @@
 const int rebootButton = 9;
 const int volumePin = 15;
 const int numberOfSensors = 1;
+const int installationStartTime = 8;
+const int installationEndTime = 18;
+const int trackGap = 500;
+const int numberOfTracks = 10;
+
 
