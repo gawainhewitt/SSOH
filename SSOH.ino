eb04a6d371769a11cfd78412c56f54cbafed9cec
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-04-26 06:26:15 +0100

    error message for outside set hours

--- SSOH.ino
+++ SSOH.ino
@@ -11,6 +11,8 @@ https://github.com/gawainhewitt
 #include "playWav.h"
 #include "mpr121.h"
 
+bool statusMessageSent = 0;
+
 void setup() {
     Wire1.begin();
     Serial.begin(57600);
@@ -27,12 +29,16 @@ void loop() {
   checkTouch();
 
   DateTime now = rtc.now();
-
-  Serial.println(now.hour(), DEC);
   
   if(now.hour() > installationStartTime - 1) {
     if(now.hour() < installationEndTime) {
       playFiles();  
+      statusMessageSent = 0;
+    } 
+  } else {
+    if(statusMessageSent == 0){
+      Serial.println("Installation off as outside set hours");
+      statusMessageSent = 1;
     }
   }
 }
